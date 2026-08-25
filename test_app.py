a739d45445a5d84d3182e54cc2beb57e62910945
Yuu <johanikaros@gmail.com>
2026-08-25 17:34:44 -0500

    vuelve a la normalidad

--- test_app.py
+++ test_app.py
@@ -2,4 +2,4 @@ from sample_app import sample
 def test_home_status_200():
     cliente = sample.test_client()
     respuesta = cliente.get('/')
-    assert respuesta.status_code == 404 # nosec B101
+    assert respuesta.status_code == 200 # nosec B101
