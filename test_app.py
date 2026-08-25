42f06fa48de37801ed55a5edf76ad8550bdd98c9
Yuu <johanikaros@gmail.com>
2026-08-25 16:46:17 -0500

    test

---
+++ test_app.py
@@ -0,0 +1,5 @@
+from sample_app import sample
+def test_home_status_200():
+    cliente = sample.test_client()
+    respuesta = cliente.get('/')
+    assert respuesta.status_code == 200 # nosec B101
